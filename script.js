4e090fa5a0609efcefeb72c92d05fdfc0c61152d
Mnde81 <minubis@gmail.com>
2024-07-11 10:56:56 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const count = document.querySelector('.count');
+const input = document.querySelector('input');
+
+
+input.addEventListener("keyup", () => {
+    const name = input.value;
+
+    count.innerHTML = input.value.length;
+})
+
